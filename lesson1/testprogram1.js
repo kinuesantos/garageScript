9064d30f373c9e5e348fa9f65fe0ea63438fc456
Kinue Santos <kinue@llip.life>
2017-08-14 18:48:28 -0700

    add to testprogram1 modify the indentation and and remove console.log line

--- lesson1/testprogram1.js
+++ lesson1/testprogram1.js
@@ -2,8 +2,9 @@ const G= require('./program1.js');
 
 const test=(a,b,c)=>{
   if(G(a,b)===c){
-  console.log('You are CORRECT');
-  }else {console.log('You are INCORRECT');
+    console.log('You are CORRECT');
+  }else{
+    console.log('You are INCORRECT');
   }
 }
 
