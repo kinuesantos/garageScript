cb1098c2991d23cdda83220013a4a9ee8a39fcdb
Kinue Santos <kinue@llip.life>
2017-08-15 09:32:00 -0700

    add testprogram1.js better names sum, input1, input2, total

--- lesson1/testprogram1.js
+++ lesson1/testprogram1.js
@@ -1,7 +1,7 @@
-const G= require('./program1.js');
+const sum= require('./program1.js');
 
-const test=(a,b,c)=>{
-  if(G(a,b)===c){
+const test=(input1,input2,total)=>{
+  if(sum(input1,input2)===total){
     console.log('You are CORRECT');
   }else{
     console.log('You are INCORRECT');
