293e55c7dd9cb849bcfc48ad109346fd52374bbf
Kinue Santos <kinue@llip.life>
2017-08-16 17:34:49 -0700

    add node.js to K2

--- lesson2/node.js
+++ lesson2/node.js
@@ -1,7 +1,18 @@
+//print out all the files in home - student names except for Kinue.
+
 const f = require('fs');
 
 const read = (err,files)=>{
-  console.log(files);
+  const notme=()=>{
+    let names = '';
+    files.forEach((e) => {
+      if(e !== 'kinue'){
+        names = '\n' + names + '\n' + e;
+      }
+    }); 
+    return names;
+  }
+  console.log(notme());
 }
-
 f.readdir('/home', read);
+
