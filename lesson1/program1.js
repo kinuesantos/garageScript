f2b5230ae85346d84298eb66209f0f7e22783526
Kinue Santos <kinue@llip.life>
2017-08-28 20:50:14 -0700

    add to program1.js - modify a and b to input1 and input2

--- lesson1/program1.js
+++ lesson1/program1.js
@@ -1,6 +1,6 @@
 //this function will add two numbers
-const adding= (a,b)=>{
-const total= a+b;
+const adding= (input1,input2)=>{
+const total= input1+input2;
   return total;
 }
 module.exports=adding;
