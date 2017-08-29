02747a20710fb60142163b2593391be7b6983cb3
Kinue Santos <kinue@llip.life>
2017-08-28 17:55:42 -0700

    add to backend28.js added a ':' in between the first and second data and a space

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -7,5 +7,5 @@ app.use(express.static('public'));
 app.get('/send',(req,res)=>{
   const firstdata = req.query.thenames;
   const seconddata = req.query.thecomments;
-  fs.writeFile('/home/kinue/garageScript/lesson4/public/storing.txt', firstdata + seconddata)
+  fs.writeFile('/home/kinue/garageScript/lesson4/public/storing.txt', firstdata + ':' + seconddata + '\n')
 });
