99275cca0bd5ce7622cf55fab635eb223767d395
Kinue Santos <kinue@llip.life>
2017-09-03 14:22:52 -0700

    add to backend28.js - modify for lesson 6 - chatroom and use appendFile, instead of writeFile.

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -7,5 +7,5 @@ app.use(express.static('public'));
 app.get('/send',(req,res)=>{
   const firstdata = req.query.userNames;
   const seconddata = req.query.userComments;
-  fs.writeFile('/home/kinue/garageScript/lesson4/public/storing.txt', firstdata + ':' + seconddata + '\n')
+  fs.appendFile('/home/kinue/garageScript/lesson4/public/storing.txt', firstdata + ':' + seconddata + '\n')
 });
