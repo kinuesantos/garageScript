24d517fa8fc433520bcb015ac1b147ee794092b5
Kinue Santos <kinue@llip.life>
2017-09-11 11:53:56 -0700

    add to backend28.js for lesson 8 webcam

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -4,10 +4,15 @@ const app = express();
 app.listen(3618);
 app.use(express.static('public'));
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '500mb'}));
 
 app.post('/send',(req,res)=>{
   const firstData = req.body.name;
   const secondData = req.body.comment;
+  const pic = req.body.newPicData;
   fs.appendFile('/home/kinue/garageScript/lesson4/public/storing.txt',`${firstData} : ${secondData}\n`)
+
+  fs.writeFile('/home/kinue/garageScript/lesson4/public/selfies.png',`${pic}`, 'base64') 
+  res.send('Pic taken');
 });
+
