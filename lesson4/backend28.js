a069a71c226fcd337a1a14dff3b9647521a68d82
Kinue Santos <kinue@llip.life>
2017-09-18 16:21:24 -0700

    add to backend28.js for lesson 9 - use the name as name of the picture and adding the comment as the meme across the pic and save it into a file (MemePhotos)

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -5,14 +5,20 @@ app.listen(3618);
 app.use(express.static('public'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '500mb'}));
+const gm = require('gm');
 
 app.post('/send',(req,res)=>{
   const firstData = req.body.name;
   const secondData = req.body.comment;
   const pic = req.body.newPicData;
-  fs.appendFile('/home/kinue/garageScript/lesson4/public/storing.txt',`${firstData} : ${secondData}\n`)
+  const storingTxt = '/home/kinue/garageScript/lesson4/public/storing.txt';
+  const memePath = '/home/kinue/garageScript/lesson4/public/MemePhotos/' + firstData + '.png';
+  
+  fs.appendFile(storingTxt,`${firstData} : ${secondData}\n`)
 
-  fs.writeFile('/home/kinue/garageScript/lesson4/public/selfies.png',`${pic}`, 'base64') 
-  res.send('Pic taken');
+  fs.writeFile(memePath, `${pic}`, 'base64', (err) =>{
+    gm(memePath).fontSize(40).drawText(50, 50, secondData).write(memePath,() =>  {})
+  console.log('Meme ERROR', err)
+    res.send('Pic saved');
+  });
 });
-
