13fdec07ee67e4b3b884886d252f0138984a0e26
Kinue Santos <kinue@llip.life>
2017-09-19 16:15:49 -0700

    add to backend28.js - made the meme photo display on the browser- K9

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -11,14 +11,17 @@ app.post('/send',(req,res)=>{
   const names = req.body.name;
   const comments = req.body.comment;
   const pic = req.body.newPicData;
-  const storingTxt = '/home/kinue/garageScript/lesson4/public/storing.txt';
   const memePath = '/home/kinue/garageScript/lesson4/public/MemePhotos/' + names + '.png';
-  
-  fs.appendFile(storingTxt,`${names} : ${comments}\n`)
+
 
   fs.writeFile(memePath, `${pic}`, 'base64', (err) =>{
-    gm(memePath).fontSize(40).drawText(50, 50, comments).write(memePath,() =>  {})
-  console.log('Meme ERROR', err)
-    res.send('Pic saved');
-  });
+    gm(memePath).fontSize(40).drawText(50, 50, comments).write(memePath, () => {
+        let currentMeme = `<img src = 'https://kinue.llip.life/MemePhotos/${names}.png'/>`;
+        res.send(currentMeme);
+      });
+    if(err!== null){
+    console.log('Meme ERROR', err)
+  }});
 });
+
+
