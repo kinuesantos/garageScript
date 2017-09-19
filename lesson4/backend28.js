99721dd8686bf35e9fd80776b32c2ba44abbfa1c
Kinue Santos <kinue@llip.life>
2017-09-19 10:20:42 -0700

    add to backend28.js changes first and second data to names and comments

--- lesson4/backend28.js
+++ lesson4/backend28.js
@@ -8,16 +8,16 @@ app.use(bodyParser.json({limit: '500mb'}));
 const gm = require('gm');
 
 app.post('/send',(req,res)=>{
-  const firstData = req.body.name;
-  const secondData = req.body.comment;
+  const names = req.body.name;
+  const comments = req.body.comment;
   const pic = req.body.newPicData;
   const storingTxt = '/home/kinue/garageScript/lesson4/public/storing.txt';
-  const memePath = '/home/kinue/garageScript/lesson4/public/MemePhotos/' + firstData + '.png';
+  const memePath = '/home/kinue/garageScript/lesson4/public/MemePhotos/' + names + '.png';
   
-  fs.appendFile(storingTxt,`${firstData} : ${secondData}\n`)
+  fs.appendFile(storingTxt,`${names} : ${comments}\n`)
 
   fs.writeFile(memePath, `${pic}`, 'base64', (err) =>{
-    gm(memePath).fontSize(40).drawText(50, 50, secondData).write(memePath,() =>  {})
+    gm(memePath).fontSize(40).drawText(50, 50, comments).write(memePath,() =>  {})
   console.log('Meme ERROR', err)
     res.send('Pic saved');
   });
