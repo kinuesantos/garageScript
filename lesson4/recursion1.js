cd186f7ef2b5ebe2b07d0f4f0eaf288159e80d99
Kinue Santos <kinue@llip.life>
2017-09-29 10:22:05 -0700

    add to recursion1.js - modify to only print and not return

--- lesson4/recursion1.js
+++ lesson4/recursion1.js
@@ -1,11 +1,9 @@
 //sayHello(5) -> 'hello' 'hello' ...x5
 
 const sayHello = (a,i=0)=>{
-  if(i==a){
-    return '';
-  }
-  console.log('hello');
-  return sayHello(a, i+1);
+  if(i<a){
+    console.log('hello');
+    sayHello(a, i+1);
 }
-
+}
 sayHello(3);
