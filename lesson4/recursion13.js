9b4630f2460942f0e29d3ce5a81f6435870591c1
Kinue Santos <kinue@llip.life>
2017-10-02 12:13:15 -0700

    add to recursion13.js - compare two arrays, if same print true, else false

---
+++ lesson4/recursion13.js
@@ -0,0 +1,13 @@
+//compareArr([1,2,3],[1,2,3]) - true
+
+const compareArr = (a,b, i=0)=>{
+  if(i== a.length && i == b.length){
+    return true;
+  }else{
+  if(a[i]!=b[i]){
+    return false;
+  }
+  return compareArr(a,b, i+1);
+}
+}
+console.log(compareArr([1,2,4],[1,2,3]));
